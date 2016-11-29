6723ecebbd405faa3c2b5c035bb9ba94c74fe628
Mike Brown <michael.brown@cfpb.gov>
2016-11-29 09:13:11 -0500

    bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django_bend",
-    description = "Database dump conversion to Django fixtures",
-    version = "0.0.3",
-    license = "MIT",
-    classifiers = [
+    name="django_bend",
+    description="Database dump conversion to Django fixtures",
+    version="0.0.4",
+    license="MIT",
+    classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
@@ -18,9 +18,9 @@ setup(
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
     ],
-    keywords = 'django fixture database dump',
-    packages = find_packages(),
-    install_requires = [
+    keywords='django fixture database dump',
+    packages=find_packages(),
+    install_requires=[
         'simplejson>=3.8.0',
     ],
 )
